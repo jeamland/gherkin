3900fbd62657365c9c6e07aa3c86be914a8d5951
Benno Rice <benno@jeamland.net>
2011-10-01 22:04:47 +1000

    Be more unicode-ish.

--- python/gherkin/sexp_recorder.py
+++ python/gherkin/sexp_recorder.py
@@ -9,22 +9,22 @@ class SexpRecorder(object):
     def __init__(self):
         self.sexps = []
 
-    comment = handle_event('comment')
-    tag = handle_event('tag')
-    feature = handle_event('feature')
-    background = handle_event('background')
-    scenario = handle_event('scenario')
-    scenario_outline = handle_event('scenario_outline')
-    examples = handle_event('examples')
-    step = handle_event('step')
-    doc_string = handle_event('doc_string')
-    row = handle_event('row')
-    eof = handle_event('eof')
-    uri = handle_event('uri')
-    syntax_error = handle_event('syntax_error')
+    comment = handle_event(u'comment')
+    tag = handle_event(u'tag')
+    feature = handle_event(u'feature')
+    background = handle_event(u'background')
+    scenario = handle_event(u'scenario')
+    scenario_outline = handle_event(u'scenario_outline')
+    examples = handle_event(u'examples')
+    step = handle_event(u'step')
+    doc_string = handle_event(u'doc_string')
+    row = handle_event(u'row')
+    eof = handle_event(u'eof')
+    uri = handle_event(u'uri')
+    syntax_error = handle_event(u'syntax_error')
 
     def errors(self):
-        return [sexp for sexp in self.sexps if sexp[0] == 'syntax_error']
+        return [sexp for sexp in self.sexps if sexp[0] == u'syntax_error']
 
     def line(self, number):
         for sexp in self.sexps:
@@ -37,9 +37,9 @@ class SexpRecorder(object):
             return [self.sexpify(event) for event in obj]
         elif type(obj) is model.Row:
             return {
-                'cells': self.sexpify(obj.cells),
-                'comments': self.sexpify(obj.comments),
-                'line': obj.line
+                u'cells': self.sexpify(obj.cells),
+                u'comments': self.sexpify(obj.comments),
+                u'line': obj.line
             }
         elif type(obj) is model.Comment:
             return obj.value
